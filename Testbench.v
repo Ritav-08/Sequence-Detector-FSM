96675569e80a91b387c7dc6701b82e4caf7a2d00
Ritav <ritav08092004@gmail.com>
2026-06-01 20:24:44 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -1,5 +1,3 @@
-//Timing error due to Non blocking at always and use at $monitor, Need to fix...
-
 `timescale 1ns / 1ps
 
 module tb_sdFSM1011 ();
@@ -16,7 +14,7 @@ sdFSM1011 DUT ( .rst_i(rst_ti),
 );
 
 //reg(s)
-reg [3:0] sequence = 4'b0;
+reg [3:0] sequence;
 
 //clock
 initial begin
@@ -26,33 +24,48 @@ end
 
 //Feeding
 initial begin
+sequence = 4'b0;
 data_ti = 1'b0;
 rst_ti = 1'b1;
-#3 rst_ti = 1'b0;
+#10 rst_ti = 1'b0;
+#10 data_ti = 1'b1; //T=20
+#10 data_ti = 1'b0;
+#10 data_ti = 1'b1;
+#10 data_ti = 1'b1;
+#10 data_ti = 1'b0;
+#10 data_ti = 1'b1;
+#10 data_ti = 1'b1;
+#10 data_ti = 1'b1;
+#10 data_ti = 1'b0;
+#10 data_ti = 1'b1;
+#10 data_ti = 1'b0;
 repeat(10) #10 data_ti = $urandom_range(0, 1);
-#10 data_ti = 1'b1;
-#10 data_ti = 1'b0;
-#10 data_ti = 1'b1;
-#10 data_ti = 1'b1;
-#10 data_ti = 1'b0;
-#10 data_ti = 1'b1;
-#10 data_ti = 1'b1;
-#10 data_ti = 1'b1;
-#10 data_ti = 1'b0;
-#10 data_ti = 1'b1;
-#10 data_ti = 1'b0;
 #5 $display ("Simulation End");
 $finish;
 end
 
-always@(posedge clk_ti) begin 
+always@(posedge clk_ti) begin
+if(rst_ti)
+   sequence <= 4'b0000;
+else begin 
 sequence <= {sequence[2:0], data_ti};
+
+#1; 
+$display("Time: %0t | Clk: %b, Rst: %b | State: %0d | Sequence: %0b | Detection: %0b", 
+          $time,      clk_ti,  rst_ti, DUT.state, sequence, dout_to);
+end
+end
+
+always@(negedge clk_ti) begin
+#1; 
+$display("Time: %0t | Clk: %b, Rst: %b | State: %0d | Sequence: %0b | Detection: %0b", 
+          $time,      clk_ti,  rst_ti, DUT.state, sequence, dout_to);
 end
 
 //capture
 initial begin
-$monitor ("Time: %0t | Clk: %b, Rst: %b | Sequence: %0b | Detection: %0b", 
-           $time,      clk_ti,  rst_ti,   sequence,       dout_to);
+//$monitor ("Time: %0t | Clk: %b, Rst: %b | Sequence: %0b | Detection: %0b", 
+//             $time,      clk_ti,  rst_ti,   {sequence[2:0], data_ti},       dout_to);
 $dumpfile("MooreFSM.vcd");
 $dumpvars(0, tb_sdFSM1011);
 end
